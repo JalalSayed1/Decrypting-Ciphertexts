94a9973432a515642bb20975020c32dc3aaebb0b
Jalal Sayed <mrjalalsayed@gmail.com>
2025-02-26 21:27:13 +0000

    Removed unused python script

--- python/find_passwords_8_chars.py
+++
@@ -1,7 +0,0 @@
-
-with open("passwords") as f:
-    lines = f.readlines()
-    for i, line in enumerate(lines):
-        if len(line) == 8:
-            with open("passwords_8_chars.txt", "a") as f:
-                f.write(line)
